59f4d0b16ad7d6bf892a6b3d26481272f2d0300d
nihal369 <nihalismailk@gmail.com>
2017-11-26 00:24:10 +0530

    JSON API call added

--- Android/app/src/main/java/com/amaro/diabeto/Predictor.java
+++ Android/app/src/main/java/com/amaro/diabeto/Predictor.java
@@ -2,7 +2,17 @@ package com.amaro.diabeto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.error.VolleyError;
+import com.android.volley.request.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Predictor extends AppCompatActivity {
 
@@ -11,13 +21,46 @@ public class Predictor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_predictor);
 
-        Log.i("NIHAL", String.valueOf(LocalDB.getAge()));
-        Log.i("NIHAL", String.valueOf(LocalDB.getBloodPressure()));
-        Log.i("NIHAL", String.valueOf(LocalDB.getDiabeticPedigree()));
-        Log.i("NIHAL", String.valueOf(LocalDB.getGender()));
-        Log.i("NIHAL", String.valueOf(LocalDB.getGlucoseLevel()));
-        Log.i("NIHAL", String.valueOf(LocalDB.getInsulinLevel()));
-        Log.i("NIHAL", String.valueOf(LocalDB.getPregnancies()));
-        Log.i("NIHAL", String.valueOf(LocalDB.getSkinThickness()));
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url;
+
+
+        url = "http://www.httpbin.org/post";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String,String> stringIntegerMap=new HashMap<>();
+                stringIntegerMap.put("pregnancies",String.valueOf(LocalDB.getPregnancies()));
+                stringIntegerMap.put("glucose",String.valueOf(LocalDB.getPregnancies()));
+                stringIntegerMap.put("bp",String.valueOf(LocalDB.getPregnancies()));
+                stringIntegerMap.put("skinThickness",String.valueOf(LocalDB.getPregnancies()));
+                stringIntegerMap.put("insulin",String.valueOf(LocalDB.getPregnancies()));
+                stringIntegerMap.put("BMI", String.valueOf(LocalDB.getPregnancies()));
+                stringIntegerMap.put("diabetesFunction", String.valueOf(LocalDB.getPregnancies()));
+                stringIntegerMap.put("age", String.valueOf(LocalDB.getPregnancies()));
+
+
+                return stringIntegerMap;
+            }
+        };
+
+        queue.add(postRequest);
     }
 }
