924e90a39e7ac1b3444f1a643e13ece61e5ddc4a
nihal369 <nihalismailk@gmail.com>
2017-11-25 15:14:37 +0530

    Interface completed

--- Interface.py
+++ Interface.py
@@ -5,18 +5,24 @@
 import numpy as np
 from keras.models import load_model
 from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
 
 
 
-def predict(int pregnancies,int glucose,int bp,int skinThickness,int insulin,float BMI,float diabetesFunction,int age):
+def predict(pregnancies, glucose, bp, skinThickness, insulin, BMI, diabetesFunction, age):
     #Initalizing the classifier
     classifier=load_model('diabetes_predictor.h5')
-    
-    
-    
+        
+    #Loading the scaler
+    scaler = joblib.load('scaler.save')
+        
     #Creating the array
-    x=np.array([[4,11,92,33,175,25.8,0.587,31]])
-    
+    x=np.array(scaler.fit_transform([[pregnancies,glucose,bp,skinThickness,insulin,BMI,diabetesFunction,age]]))
+        
     #Predicting the output
     ypred=classifier.predict(x)
     
+    #Return the value
+    return ypred
+
+
