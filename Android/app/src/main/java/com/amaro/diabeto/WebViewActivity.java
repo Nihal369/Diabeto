44b0aa967a0589e158dc4958dbb69599dbff95d0
nihal369 <nihalismailk@gmail.com>
2017-11-26 05:08:53 +0530

    Premium content added

---
+++ Android/app/src/main/java/com/amaro/diabeto/WebViewActivity.java
@@ -0,0 +1,22 @@
+package com.amaro.diabeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        WebView webview = findViewById(R.id.webView);
+        webview.loadUrl("https://www.prolonfmd.com/fasting-mimicking-diet/");
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebChromeClient(new WebChromeClient());
+        webview.setWebViewClient(new WebViewClient());
+
+    }
+}
