2c67a9b16db7411c668b29b47cac92f6a0b77b0b
nihal369 <nihalismailk@gmail.com>
2017-11-25 14:56:10 +0530

    Interface in more progress

--- Interface.py
+++ Interface.py
@@ -4,14 +4,19 @@
 
 import numpy as np
 from keras.models import load_model
+from sklearn.preprocessing import StandardScaler
 
 
-#Initalizing the classifier
-classifier=load_model('diabetes_predictor.h5')
 
-#Creating the array
-x=np.array([[4,11,92,33,175,25.8,0.587,31]])
-
-#Predicting the output
-ypred=classifier.predict(x)
-
+def predict(int pregnancies,int glucose,int bp,int skinThickness,int insulin,float BMI,float diabetesFunction,int age):
+    #Initalizing the classifier
+    classifier=load_model('diabetes_predictor.h5')
+    
+    
+    
+    #Creating the array
+    x=np.array([[4,11,92,33,175,25.8,0.587,31]])
+    
+    #Predicting the output
+    ypred=classifier.predict(x)
+    
