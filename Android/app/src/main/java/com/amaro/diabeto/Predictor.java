e8695d62dfa6d1b4012bc46ad429b16be1785200
nihal369 <nihalismailk@gmail.com>
2017-11-26 07:18:55 +0530

    Too much changes in one commit

--- Android/app/src/main/java/com/amaro/diabeto/Predictor.java
+++ Android/app/src/main/java/com/amaro/diabeto/Predictor.java
@@ -1,5 +1,6 @@
 package com.amaro.diabeto;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,8 +10,17 @@ import com.android.volley.Response;
 import com.android.volley.error.VolleyError;
 import com.android.volley.request.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.daasuu.cat.CountAnimationTextView;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -62,5 +72,27 @@ public class Predictor extends AppCompatActivity {
         };
 
         queue.add(postRequest);
+
+        int percentage=75;
+
+        CountAnimationTextView mCountAnimationTextView = findViewById(R.id.percentageText);
+        mCountAnimationTextView
+                .setAnimationDuration(1500)
+                .countAnimation(0,percentage);
+
+        PieChart pieChart =  findViewById(R.id.pieChart);
+        List<PieEntry> entries=new ArrayList<>();
+        entries.add(new PieEntry(percentage,0));
+        entries.add(new PieEntry((100-percentage),1));
+        PieDataSet dataSet=new PieDataSet(entries,"");
+        dataSet.setColors(Color.WHITE,Color.parseColor("#bdbdbd"));
+        PieData pieData=new PieData(dataSet);
+        pieChart.setData(pieData);
+        pieChart.setHoleRadius(90);
+        pieChart.getDescription().setEnabled(false);
+        pieData.setDrawValues(false);
+        pieChart.getLegend().setEnabled(false);
+        pieChart.setHoleColor(Color.TRANSPARENT);
+        pieChart.animateY(1500);
     }
 }
